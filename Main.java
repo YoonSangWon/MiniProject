dc79b782c47c6b12e63abfbf0b1641e21af99724
SangWon-Yoon <yswon93@naver.com>
2018-03-13 16:06:52 +0900

    V1

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+
+        list.add('c');
+
+        System.out.print(list + "\n");
+    }
+}
